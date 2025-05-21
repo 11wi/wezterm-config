2f2f8776ed760da3df4aca6526c9e951d1464970
wochoi119 <angju8@gmail.com>
2025-05-21 09:40:20 +0900

    Update domains.lua

--- config/domains.lua
+++ config/domains.lua
@@ -1,28 +1,33 @@
+local wezterm = require('wezterm')
+
+-- Host devjump
+--     include include/jumpbox-dev.conf
+--     proxycommand ssh -W %h:%p kko
+--     UserKnownHostsFile ~/.ssh/jumphost
+
+local ssh_domains = {
+  {
+    name = 'dev',
+    remote_address = 'devjump',
+    multiplexing = "None",
+    assume_shell = 'Posix',
+    default_prog = {'bash'},
+  },  
+}
+ 
+
 return {
    -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
    -- ssh_domains = {},
-   ssh_domains = {
-      -- yazi's image preview on Windows will only work if launched via ssh from WSL
-      {
-         name = 'wsl.ssh',
-         remote_address = 'localhost',
-         multiplexing = 'None',
-         default_prog = { 'fish', '-l' },
-         assume_shell = 'Posix'
-      }
-   },
+   ssh_domains = ssh_domains,
+
+   -- ssh_backend="Ssh2",
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
    unix_domains = {},
 
    -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
    wsl_domains = {
-      {
-         name = 'WSL:Ubuntu',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish', '-l' },
-      },
+      
    },
 }
